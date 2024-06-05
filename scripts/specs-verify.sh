cacd3f74ce160d3c6ad7c93d4a47425e81fd0cf3
Melvin Lee <melvin.lee@iswish.nl>
2024-06-05 08:14:03 +0200

    ci(specs-verify.sh): refactoring tbv wijzigen van parameters via commandline

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PARAMS="{ \
+    \"logFileToAssert\": \"./test-data/logs/reisdocumenten-informatie-service.json\", \
+    \"oAuth\": { \
+        \"enable\": false \
+    } \
+}"
+
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
@@ -10,4 +17,5 @@ npx cucumber-js -f json:./test-reports/cucumber-js/reisdocumenten/test-result.js
                 -f summary:./test-reports/cucumber-js/reisdocumenten/test-result-summary.txt \
                 -f summary \
                 features/raadpleeg-met-reisdocumentnummer \
-                features/zoek-met-burgerservicenummer
+                features/zoek-met-burgerservicenummer \
+                --world-parameters "$PARAMS"
