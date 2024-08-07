71e384d83c1adc2db073688018c9b6fd61910b2f
Melvin Lee <melvin.lee@iswish.nl>
2024-08-07 10:31:23 +0200

    ci: release multi-platform container images stappen toegevoegd

--- scripts/containers-start.sh
+++ scripts/containers-start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+MODE=$1
+
+if [ "$MODE" = "ci" ]; then
+    docker compose -f .docker/db-ci.yml up -d
+else
+    docker compose -f .docker/db.yml up -d
+fi
+
 docker compose \
-    -f .docker/db.yml \
     -f .docker/reisdocumenten-data-service.yml \
     -f .docker/reisdocumenten-informatie-service.yml \
     up -d
--- scripts/containers-stop.sh
+++ scripts/containers-stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+MODE=$1
+
+if [ "$MODE" = "ci" ]; then
+    docker compose -f .docker/db-ci.yml down --volumes
+else
+    docker compose -f .docker/db.yml down --volumes
+fi
+
 docker compose \
-    -f .docker/db.yml \
     -f .docker/reisdocumenten-data-service.yml \
     -f .docker/reisdocumenten-informatie-service.yml \
-    down
+    down --volumes
