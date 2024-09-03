1e14bc6ccfb52303d2824632f14b59bf35dfffb5
Melvin Lee <melvin.lee@iswish.nl>
2024-09-03 08:36:32 +0200

    chore(ci): data-service manifest updated ivm nieuwe versie

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PARAMS="{ \
+    \"apiUrl\": \"http://localhost:5002/haalcentraal/api\", \
     \"logFileToAssert\": \"./test-data/logs/reisdocumenten-informatie-service.json\", \
     \"oAuth\": { \
         \"enable\": false \
